48a14c55fe8e3fc26e5b444b598cefc511c95388
Lhyebin <133613625+Lhyebin@users.noreply.github.com>
2023-08-25 16:20:57 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $(function () {
+    //@media : header 메뉴바
+    $('.mopen').on('click', function () {
+        $('.bottom_left').toggleClass('on')
+    })
+
+    $('.bottom_left>ul>li>a').on('click', function (e) {
+        e.preventDefault();
+        $(this).next().slideToggle();
+        $(this).parent().siblings().find('.sub').slideUp();
+        // -> 마지막줄 반응형에서 다른 서브메뉴 클릭했을때 이전 서브메뉴 들어가게하기
+    })
+
+
+
+
     // 메인비쥬얼 : 슬라이드 스테이 타임
     $('.visual_wrap').on('init afterChange', function (e, s, c) {
         $('.stay_bar span').animate({ width: ((100 / 6) * c) + 16.66666666 + "%" }, 400)
@@ -7,7 +22,7 @@ $(function () {
     $('.visual_wrap').slick({
         arrows: false,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 3500,
         speed: 1000,
         pauseOnHover: false,
         pauseOnFocus: false,
@@ -38,6 +53,16 @@ $(function () {
         autoplay: true,
         autoplaySpeed: 3000,
         speed: 1000,
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    // slidesToScroll: 1,
+                }
+            },
+        ]
     });
 
     $('.product_wrap .product_arrows .left').on('click', function () {
@@ -61,7 +86,24 @@ $(function () {
         autoplay: true,
         autoplaySpeed: 2000,
         speed: 1000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    // slidesToScroll: 1,
+                }
+            },
+        ]
     });
+
+    //@media 반응형 content:화살표
+    $('.content_wrap .content_mopen .left').on('click', function () {
+        $('.content_cap').slick('slickPrev')
+    })
+    $('.content_wrap .content_mopen .right').on('click', function () {
+        $('.content_cap').slick('slickNext')
+    })
 
 
 
